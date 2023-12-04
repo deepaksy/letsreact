59f6641a28eb22d5b40a4fd51d7fe3374879144e
deepaksy <deepaksy28@gmail.com>
2023-12-04 18:23:39 +0530

    update

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,21 @@
 import useTheme from '../../helpers/useTheme'
 import Button from '../Button'
 import './index.scss'
+import ReactLogo from '../../assets/react.svg'
 
 const Navbar = () => {
   const {isDark,toggleTheme} = useTheme()
   return (
-    <nav>
+    <nav className='navbar-container'>
+      <div className='navbar-left'>
+        <img src={ReactLogo} alt='react-logo' className='react-logo' />
+      </div>
+      <div>
+        searchbar
+      </div>
+      <div className='navbar-right'>
       <Button  onClick={()=>toggleTheme()}>{isDark?'light':'dark'}</Button>
+      </div>
     </nav>
   )
 }
