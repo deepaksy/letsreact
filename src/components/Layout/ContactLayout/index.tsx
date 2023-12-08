e178c590c71904579a195bb1d16901cc34eb8354
deepaksy <deepaksy28@gmail.com>
2023-12-08 23:35:05 +0530

    Add contact layout

---
+++ src/components/Layout/ContactLayout/index.tsx
@@ -0,0 +1,11 @@
+import './index.scss'
+
+const ContactLayout = ({children}:{children:React.ReactNode}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default ContactLayout
