e627b8bbb4f1ac2b8dbc8ffaf40390b09a7be285
deepaksy <deepaksy28@gmail.com>
2023-12-08 17:28:51 +0530

    Update Layout for AppRouter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,28 +2,28 @@ import
 {
 BrowserRouter as Router,
 Routes,
-Route
+Route,
 } from 'react-router-dom'
 import App from './App'
 
 import About from './pages/About'
 import Navbar from './components/Navbar'
 import Homepage from './pages/Homepage'
-import DocLayout from './components/DocLayout'
 import Test from './pages/Test'
+import Layout from './components/Layout'
 
 const AppRouter = () => {
   return (
     <Router>
         <Navbar/>
-        <DocLayout>
+        <Layout>
         <Routes>
             <Route path='/' element={<App/>} />
-            <Route path='/docs' element={<Homepage/>} />
+            <Route path='/docs' element={<Homepage/>}/>
             <Route path='/about' element={<About/>} />
             <Route path='/test' element={<Test/>} />
         </Routes>   
-        </DocLayout>
+        </Layout>
     </Router>
   )
 }
