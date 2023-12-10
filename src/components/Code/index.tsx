9ffa24c6d4fef2ef8276269cb16d2f407f808cdf
deepaksy <deepaksy28@gmail.com>
2023-12-10 22:54:11 +0530

    Update Code component

--- src/components/Code/index.tsx
+++ src/components/Code/index.tsx
@@ -1,10 +1,22 @@
+import Toast from '../Toast';
 import './index.scss'
-import { useLayoutEffect, useRef } from 'react'
+import { useLayoutEffect, useRef, useState } from 'react'
 
 
-const Code = ({children}:{children:string}) => {
+const Code = ({children}:{children:any}) => {
+  const [showToast,setShowToast] = useState(false);
+  const content = children.props.children.props.children;
+  const handleCopy = () => {
+    navigator.clipboard.writeText(content)
+    setShowToast(true)
+  }
+
   return (
-    <>{children}</>
+    <div className='code-container'>
+    {children}
+    <button className='copy-button' onClick={()=> handleCopy()} >copy to clipboard</button>
+    {showToast && <Toast message='content copied to your clipboard' />}
+    </div>
   )
 }
 
