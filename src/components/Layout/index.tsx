81dc1e4849a2f4ed86eccfb993a069515705e331
deepaksy <deepaksy28@gmail.com>
2023-12-12 22:44:52 +0530

    Added ::before padding to Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,28 +1,40 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import { useLocation } from 'react-router-dom'
 import DocLayout from './DocLayout'
 import ContactLayout from './ContactLayout'
 import AboutLayout from './AboutLayout'
+import './index.scss'
 
 const Layout = ({children}:{children:React.ReactNode}) => {
     const location = useLocation()
     if(location.pathname.includes('/docs')){
         return (
-            <DocLayout>{children}</DocLayout>
+            <Spacer>
+                <DocLayout>{children}</DocLayout>
+            </Spacer>
           )
     }
     else if(location.pathname.includes('/')){
-        return <main>{children}</main>
+        return <Spacer><main>{children}</main></Spacer>
     }
     else if(location.pathname.includes('/contact')) {
-        return <ContactLayout>{children}</ContactLayout>
+        return <Spacer><ContactLayout>{children}</ContactLayout></Spacer>
     }
     else if(location.pathname.includes('/about')) {
-        return <AboutLayout>{children}</AboutLayout>
+        return <Spacer><AboutLayout>{children}</AboutLayout></Spacer>
     }
     else {
-        return <>{children}</>
+        return <Spacer><>{children}</></Spacer>
     }
+}
+
+
+const Spacer = ({children}:{children: ReactElement}) => {
+    return (
+        <div className='spacer'>
+            {children}
+        </div>
+    )
 }
 
 export default Layout
